13e86f45a73e164f68ea0a0cd6ea4f92ddca1d74
philip <ajibolaphilip10@gmail.com>
2024-08-24 14:53:00 +0100

    i created .env file to put my database configurations

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('questionnaire_db', 'root', 'Bobbyjay1@2&3', {
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
 });
